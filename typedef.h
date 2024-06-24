ae21c70e4e68b8a83f859afe08faf87be0e4cc51
Farah Ossama Abdelkerim <117786011+farrahossama@users.noreply.github.com>
2024-06-24 09:16:32 +0300

    Update typedef.h

--- typedef.h
+++ typedef.h
@@ -13,22 +13,6 @@ typedef signed   short int s16;
 typedef signed   long  int s32;
 typedef float              f32;
 typedef double             f64;
-typedef struct {
-    u8 name[10];
-    u16 ID;
-    u8 gender;
-    u16 age;
-} patient;
 
-typedef struct
-{
-    char s[15];
-    char status[15];
-    u8 id;
-}slots;
-
-#define MAX_ID 20
-#define ID_DEFAULT 30
-#define SLOTS_NUMBER 5
 
 #endif
